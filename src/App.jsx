57aa4fdda0d29365867f0681ad5f3268de70f2fb
https://github.com/priyakapoor13/kishori-madhav-creations <priyakapoor2200@gmail.com>
2025-05-11 12:54:01 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar"
+import NewsBoard from "./Components/NewsBoard"
+import Footer from "./Components/footer";
+
+
+
+const App = () => {
+  const [category, setCategory] = useState("general");
+
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar setCategory={setCategory} />
+      <div className="flex-grow-1">
+        <NewsBoard category={category} />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+
+
+export default App
